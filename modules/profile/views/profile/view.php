f7d3ee26ba16bd609671ec2abc44296f25376e61
Artur1312 <arturplay007@gmail.com>
2018-03-14 19:13:28 +0200

    user wallet in profile view adde

--- modules/profile/views/profile/view.php
+++ modules/profile/views/profile/view.php
@@ -1,5 +1,7 @@
 <?php
 
+use yii\data\ActiveDataProvider;
+use yii\grid\GridView;
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 
@@ -23,6 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'method' => 'post',
             ],
         ]) ?>
+        <?= Html::a('Create Wallet', ['/wallet/create', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
     </p>
 
     <?= DetailView::widget([
@@ -61,6 +64,77 @@ $this->params['breadcrumbs'][] = $this->title;
             'interests',
         ],
     ]) ?>
+
+    <h3>User's Wallets</h3>
+
+    <?= GridView::widget([
+        'dataProvider' => new ActiveDataProvider(['query' => $model->user->getWallet()]),
+        'layout' => "{items}\n{pager}",
+        'columns' => [
+            'id',
+            'description',
+            [
+                /**
+                 * Название поля модели
+                 */
+                'attribute' => 'isActive',
+                'label' => 'status',
+                /**
+                 * Формат вывода.
+                 * В этом случае мы отображает данные, как передали.
+                 * По умолчанию все данные прогоняются через Html::encode()
+                 */
+                'format' => 'raw',
+                /**
+                 * Переопределяем отображение фильтра.
+                 * Задаем выпадающий список с заданными значениями вместо поля для ввода
+                 */
+                'filter' => [
+                    0 => 'De-activated',
+                    1 => 'Activated',
+                ],
+                /**
+                 * Переопределяем отображение самих данных.
+                 * Вместо 1 или 0 выводим Yes или No соответственно.
+                 * Попутно оборачиваем результат в span с нужным классом
+                 */
+                'value' => function ($model, $key, $index, $column) {
+                    $active = $model->{$column->attribute} === 1;
+                    return \yii\helpers\Html::tag(
+
+                        'span',
+                        $active ? 'Activated' : 'De-Activated',
+
+
+                        [
+                            'class' => 'alert alert-' . ($active ? 'success' : 'danger'),
+
+                        ]
+
+                    );
+                },
+            ],
+            [
+                'header'=>'Actions',
+                'class'=>'app\widgets\CustomColumn',
+                'controller'=>'/wallet'
+            ],
+            [
+                'label' => 'Activation',
+                'format' => 'html',
+                'value' => function($model) {
+                    if($model->isActive == 1){
+//                        return Html::checkbox('Click me', ['wallet/disactivate'], ['class'=> 'toggle','data-toggle'=> 'switch', 'data-on-color'=>"info" , 'data-off-color'=>'info', 'data-on-text'=>"", 'data-off-text'=>"",'data-pjax' => 0]);
+                        return Html::a('De-activate', ['/wallet/disactivate', 'id'=>$model->id], ['class' => 'btn btn-danger btn-xs']);
+                    } else {
+                        return Html::a('Activate', ['/wallet/activate', 'id'=>$model->id], ['class' => 'btn btn-success btn-xs']);
+
+//                        return 0;
+                    }// render your custom button
+                }
+            ]
+        ],
+    ]); ?>
 
     <?php echo \yii2mod\comments\widgets\Comment::widget([
         'model' => $model,
