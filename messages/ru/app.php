e497cc19a6fd0a91d7cdb32bf01e64d0b7ba0e54
ArtiAiry <workairspace@gmail.com>
2018-03-31 14:57:25 +0300

    app-translate

---
+++ messages/ru/app.php
@@ -0,0 +1,71 @@
+<?php
+
+
+
+return [
+
+    //sidebar
+
+    'MP'=>'МП',
+    'EP'=>'РП',
+    'My Profile'=>'Мой Профиль',
+    'Edit Profile'=>'Редактировать',
+    'Lead Info'=>'Лиды',
+    'Order Info'=>'Заказы',
+    'Products'=>'Продукты',
+    'Wallets'=>'Кошельки',
+    'Payouts'=>'Выплаты',
+    'Payments'=>'Оплаты',
+    'Tutors'=>'Преподаватели',
+    'Comments'=>'Комментарии',
+    'Events'=>'События',
+
+    //navbar
+
+    'Create'=>'Создать',
+    'Manage'=>'Управлять',
+    'Do Nothing'=>'Нет экшена',
+    'Submit to live'=>'Подтвердить',
+    'Another action'=>'Другое действие',
+    'Help Center'=>'Справка',
+    'Settings'=>'Настройки',
+    'Lock'=>'Блокировать',
+    'Log out'=>'Выйти',
+
+
+    //footer
+
+    'Home'=>'На главную',
+    'About'=>'О программе',
+    'Blog'=>'Блог',
+    'Soon'=>'Скоро',
+    'Our Website'=>'Наш сайт',
+
+    //other
+
+    'Login'=>'Логин',
+    'Log in'=>'Вход',
+    'Log in '=>'Войти',
+    'Sign up'=>'Регистрация',
+    'Register'=>'Зарегистрироваться',
+    'Back to home'=>'Вернуться на главную',
+    'Error'=>'Ошибка',
+    'Enter Username or Email'=>'Введите Логин или Почту',
+    'Enter Password'=>'Введите Пароль',
+    'Password'=>'Пароль',
+    'Your Username'=>'Ваш логин',
+    'Your Email'=>'Ваша почта',
+    'Your Password'=>'Ваш пароль',
+    'Repeat Password'=>'Повторите пароль',
+    'Create Account'=>'Создать аккаунт',
+    'Lock Screen'=>'Экран Блокировки',
+    'Unlock'=>'Разблокировать',
+    'Test'=>'Тест'
+
+
+
+
+
+];
+
+?>
